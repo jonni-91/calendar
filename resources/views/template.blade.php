258094fc0028dcef353f83fa39ba2bbc0ff0d55c
Evgenii <sk.zav@mail.ru>
2021-08-27 17:57:09 +0300

    добавлены версии для js/css файлов

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ mix('/css/app.css') }}">
     <title>Laravel</title>
 </head>
 <body>
